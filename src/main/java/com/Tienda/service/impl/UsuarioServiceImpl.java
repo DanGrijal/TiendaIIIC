1bf46d6e6bc7efc64cda00a7dfe42af856e4d305
Danny Enrique Grijalba Gutiérrez <danny.grijal1999@outlook.com>
2023-11-21 15:57:12 -0600

    Práctica Semana 10 - Commit Semanal: Usuarios

---
+++ src/main/java/com/Tienda/service/impl/UsuarioServiceImpl.java
@@ -0,0 +1,64 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.Tienda.service.impl;
+
+import com.Tienda.dao.UsuarioDao;
+import com.Tienda.domain.Rol;
+import com.Tienda.domain.Usuario;
+import com.Tienda.service.UsuarioService;
+import jakarta.servlet.http.HttpSession;
+import java.util.ArrayList;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ *
+ * @author danny
+ */
+@Service("UserDetailsService")
+public class UsuarioServiceImpl implements UsuarioService, UserDetailsService{
+    
+    @Autowired
+    private UsuarioDao UsuarioDao;
+
+    @Autowired
+    private HttpSession session;
+    
+    @Override
+    @Transactional(readOnly = true)
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        //Buscar al usuario por el username en la BD
+        Usuario usuario = UsuarioDao.findByUsername(username);
+        //si no exuste el suaurio lanza una excepcion
+        if (usuario == null) {
+            throw new UsernameNotFoundException(username);
+        }
+        //Si lñegó hasta acá es porque el usaurio existe en la BD
+        //Remover atributos de la sesión
+        session.removeAttribute("usuarioImagen");
+        session.setAttribute("usuarioImagen", usuario.getRutaImagen());
+        //Transformar roles a GrantedAuthority
+        var roles = new ArrayList<GrantedAuthority>();
+        for (Rol item : usuario.getRoles()) {
+            roles.add(new SimpleGrantedAuthority(item.getNombre()));
+        }
+        
+        //Se retorna el User (Clase UserDetails)
+        return new User(usuario.getUsername(), usuario.getPassword(), roles);
+    }
+
+    @Override
+    public Usuario getUsuarioPorUsername(String username) {
+        return UsuarioDao.findByUsername(username);
+    }
+    
+}
